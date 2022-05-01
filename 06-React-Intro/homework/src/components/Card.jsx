3e85c7f59ea80d4af3290961b88c3eeacffd1484
federicofucci21 <federicofucci@hotmail.com>
2022-05-01 15:45:21 -0300

    rock

--- 06-React-Intro/homework/src/components/Card.jsx
+++ 06-React-Intro/homework/src/components/Card.jsx
@@ -2,5 +2,22 @@ import React from 'react';
 
 export default function Card(props) {
   // acá va tu código
-  return <div>Card Component</div>
+  return (
+  <div>
+    <div>
+      <h2>{props.name}</h2>
+    </div>
+    <button onClick={props.onClose}>X</button>
+    <div>
+      <h5>Min</h5>
+      <h5>{props.min}</h5>
+    </div>
+    <div>
+      <h5>Max</h5>
+    <h5>{props.max}</h5>
+    </div>
+    <img src={`http://openweathermap.org/img/wn/${props.img}@2x.png`} alt='Image Not Found' />
+  </div>
+)
+  
 };
--- 06-React-Intro/homework/src/components/SearchBar.jsx
+++ 06-React-Intro/homework/src/components/SearchBar.jsx
@@ -2,5 +2,10 @@ import React from 'react';
 
 export default function SearchBar(props) {
   // acá va tu código
-  return <div>Search Bar Component</div>
+  return (
+  <div>
+    <input type="Text" name="" id="" />
+    <button onClick={()=>props.onSearch('Denver')}>Agregar</button>
+    </div>
+  )
 };
